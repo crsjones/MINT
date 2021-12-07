0e0bc134ae7a4fc3d3f73851eb34aac83c6083ab
crsjones <40134946+crsjones@users.noreply.github.com>
2021-12-07 21:37:11 +1100

    Added MINT Files

---
+++ MINT-macros.asm
@@ -0,0 +1,19 @@
+empty_:
+        .cstr ";"
+
+backsp_:
+        .cstr "\\c@0=0=(1_\\c\\+8\\E` `8\\E);"
+
+edit_:
+        .cstr "`?`\\K\\N`> `\\^A-\\Z;"
+
+list_:
+        .cstr "\\N26(\\i@\\Z\\c@0>(\\N))\\N`> `;"
+
+printStack_:
+        .cstr "`=> `\\P\\N\\N`> `;"        
+
+toggleBase_:
+        .cstr "\\b@0=\\b!;"
+
+
